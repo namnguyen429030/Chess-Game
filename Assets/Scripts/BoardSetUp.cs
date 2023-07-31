5d11e28ab1f6cddd883ae4b3e27728564a5abeb0
Mosena <anemosnguyen2409@gmail.com>
2023-07-31 10:07:56 +0700

    place pieces successfully

--- Assets/Scripts/BoardSetUp.cs
+++ Assets/Scripts/BoardSetUp.cs
@@ -5,34 +5,38 @@ using UnityEngine;
 
 public class BoardSetUp : MonoBehaviour
 {
-    public GameObject WhiteTile, BlackTile, WhiteKing, BlackKing, WhiteQueen, BlackQueen, WhiteKnight, BlackKnight, WhiteBishop, BlackBishop, WhiteRook, BlackRook, WhitePawn, BlackPawn;
-    public Dictionary<GameObject, GameObject> Tiles { get; private set; }
+    public GameObject WhiteTile, BlackTile;
     private const float TILE_HEIGHT = 0.5f;
     private const float TILE_WIDTH = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        Tiles = new Dictionary<GameObject, GameObject>();
-        for (char i = 'a'; i < 'i'; i++)
-        {
-            float x = i - 104;
-            for(int j = 1;j < 9; j++)
-            {
-                float y = (float)(j - 4);
-                if (((int)i % 2 == 0 && j % 2 == 0) || ((int)i % 2 != 0 && j % 2 != 0)) {
-                    Tiles.Add(Instantiate(BlackTile, new Vector3(x - TILE_WIDTH, y - TILE_HEIGHT, 0), Quaternion.identity), null);
-                }
-                else
-                {
-                    Tiles.Add(Instantiate(WhiteTile, new Vector3(x - TILE_WIDTH, y - TILE_HEIGHT, 0), Quaternion.identity), null);
-                }
-            }
-        }
+        SetUpBoard();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void SetUpBoard()
+    {
+        for (char i = 'a'; i < 'i'; i++)
+        {
+            float x = i - 104;
+            for (int j = 1; j < 9; j++)
+            {
+                float y = (float)(j - 4);
+                if (((int)i % 2 == 0 && j % 2 == 0) || ((int)i % 2 != 0 && j % 2 != 0))
+                {
+                    BlackTile = Instantiate(BlackTile, new Vector3(x - TILE_WIDTH, y - TILE_HEIGHT, 0), Quaternion.identity);
+                    BlackTile.name = i + "" + j;
+                }
+                else
+                {
+                    WhiteTile = Instantiate(WhiteTile, new Vector3(x - TILE_WIDTH, y - TILE_HEIGHT, 0), Quaternion.identity);
+                    WhiteTile.name = i + "" + j;
+                }
+            }
+        }
     }
 }
